b018f0be067d2da9a7723b55cc27c86b91cb2fa0
jkeychan <jkeychan@ibiblio.org>
2020-08-18 17:01:42 -0400

    made new functions: get_current_mac and changed answer regex

--- macchanger.py
+++ macchanger.py
@@ -3,16 +3,15 @@
 # subprocess for the shell commands like ifconfig
 # optparse for the command line help and arguments
 # sys.exit for a clean ending and restarting the program in the loop
-# platform to understand which command to use
-# match for the regular expressions in the loops below
+# re for the regular expressions and matching in MAC address and user answers
+# time for basic sleep after restarting the network interface
 
 import subprocess
 import optparse
 import sys
 import os
-import platform
-from re import match
-
+import re
+import time
 
 
 def get_arguments():
@@ -22,50 +21,39 @@ def get_arguments():
     cli_parser = optparse.OptionParser()
     cli_parser.add_option("-i", "--interface", dest="interface", help="Interface to change.")
     cli_parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address.")
-    if not options.interface: # if not True (value not set) then...
+    (options, arguments) = cli_parser.parse_args()
+    if not options.interface:  # if not True (value not set) then...
         cli_parser.error('[-] Please specify an interface name (ex. eth0), use --help for more info')
     elif not options.new_mac:
         cli_parser.error('[-] Please specify an MAC Address (ex. d6:1a:dd:85:2a:f9), use --help for more info')
     return options
 
+
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
+
+
 def change_mac(interface, new_mac):
-    print("\nChanging MAC address to " + new_mac + " ...\n")
+    current_mac = get_current_mac(options.interface)
+    print("\nChanging MAC address from " + current_mac + " to " + new_mac + " ...\n")
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     print("\nBringing " + interface + " back up now... \n")
+    time.sleep(2)
     subprocess.call(["ifconfig", interface, "up"])
-    print("Done! \n" + new_mac + " has been set on " + interface + "\n\n")
-    subprocess.call(["ip ", "link ", "show", interface])
-    print()
+
 
 # Runs the get_arguments function and returns values to pass to the next function to initialize options and arguments
-
 (options) = get_arguments()
 
-
-
-# First command to read the network interface details
-# command1 = ['ifconfig'] # Linux
-# command1 = ['ipconfig'] # OSX/Darwin
-
-if platform.system == "Darwin":
-    command1 = ['ipconfig']
-else:
-    command1 = ['ifconfig']
-
-# Take stdout from command1 and put into a pipe
-
-command1.append(options.interface)
-process1 = subprocess.Popen(command1, stdout=subprocess.PIPE)
-
-# Second command to grep for the MAC/hw address and initialize as current_mac
-# Takes stdout from process1 and puts into the pipe
-
-command2 = ['grep', '-o', '-E', '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}']
-process2 = subprocess.Popen(command2, stdin=process1.stdout, stdout=subprocess.PIPE)
-
-# Initialize current_mac variable
-(current_mac, err) = process2.communicate()
+# Brings current_mac out of the function for others to use
+current_mac = get_current_mac(options.interface)
 
 # User interaction
 
@@ -73,23 +61,15 @@ print("\n [+] Original MAC address of " + options.interface + " is: " + str(curr
 print("\n WARNING - This will restart your network interface and you might temporarily lose connectivity \n")
 answer = input("\n Change MAC address to " + options.new_mac + " (y/N) ?  \n")
 
-# Use regex from re to ensure only one character of a-z can be accepted, else restart the program
+# Use regex from re to ensure only Y or N can be accepted, else restart the program. If N, then exit
 
-if not match("^[a-z]*$", answer):
-
+if not re.match(r"[Yy]|[Nn]", answer):
     print("Invalid input. Please enter 'y' or 'n' \n")
     os.execl(sys.executable, sys.executable, *sys.argv)  # restarts the whole program on bad input
 
-elif answer == "y":
-    change_mac(options.interface,options.new_mac)  # custom function change_mac to simplify readability
-
-elif answer == "":
-    print("Nevermind. Exiting... \n")
-
-elif answer == "n":
-    print("Nevermind. Exiting... \n")
-    sys.exit()
+elif re.match(r"[Yy]", answer):
+    change_mac(options.interface, options.new_mac)  # custom function change_mac to simplify readability
 
 else:
-    print("Invalid input. Please enter 'y' or 'n' \n")
-    os.execl(sys.executable, sys.executable, *sys.argv)
+    print("Nevermind. Exiting... \n")
+    sys.exit()
